6825077362f2b185b2711e2223f7279799d6132e
Pablo <pablocavalcantee@gmail.com>
2026-04-22 19:40:48 -0300

    currículo att

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
 
         <HeaderSocials/>
 
-        <a href="/cvpablocavalcante.pdf" download="curriculo_pablopcavalcante.pdf" className={styles.btn}>
+        <a href="/cvatt_pablocavalcante_.pdf" download="curriculo_pablopcavalcante.pdf" className={styles.btn}>
           Download CV
           <FaCloudDownloadAlt className={styles.icon_cv}/>
         </a>
